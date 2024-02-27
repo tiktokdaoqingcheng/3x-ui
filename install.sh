a0a0414163104db959f3072ec02b61913563b2cd
tiktokdaoqingcheng <161429204+tiktokdaoqingcheng@users.noreply.github.com>
2024-02-27 18:52:49 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -98,7 +98,7 @@ install_base() {
 
 # This function will be called when user installed x-ui out of security
 config_after_install() {
-    echo -e "${yellow}Install/update finished! For security it's recommended to modify panel settings ${plain}"
+    echo -e "${yellow}出于安全考虑，安装/更新完成后需要强制修改端口与账户密码 ${plain}"
     read -p "Do you want to continue with the modification [y/n]?": config_confirm
     if [[ "${config_confirm}" == "y" || "${config_confirm}" == "Y" ]]; then
         read -p "Please set up your username:" config_account
